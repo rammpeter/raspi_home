1a761d6d733ac84e8b4c9db682d65ffe9f0808a7
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-10-04 19:08:55 +0200

    Einbindung Sensor

--- app/models/temperatur.rb
+++ app/models/temperatur.rb
@@ -1,9 +1,19 @@
 class Temperatur < ActiveRecord::Base
 
+  # gibt die im File hinterlegte Temperatur zurück
+  def read_temperature_from_file(env_name)
+    raise "#{env_name} muss mit Pfad und Name der w1_slave-Datei belegt sein für den Sesnor"     unless ENV[env_name]
+
+    lines = IO.readlines(ENV[env_name])
+    raise "2 Zeilen werden erwartet in File #{ENV[env_name]} statt #{lines.count}" if lines.count != 2
+    line = lines[1]
+    line.slice(line.index('t=')+2, 5).to_f/1000   # Temoeratur in Grad
+  end
+
   def self.schreibe_temperatur(vorlauf, ruecklauf)
     t = Temperatur.new(
-        :Vorlauf    => vorlauf,
-        :Ruecklauf  => ruecklauf
+        :Vorlauf    => read_temperature_from_file('FILENAME_VORLAUF'),
+        :Ruecklauf  => read_temperature_from_file('FILENAME_RUECKLAUF')
     ).save
   end
 end
