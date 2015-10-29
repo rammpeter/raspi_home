7ea2482f46f4e29a3f462d687bec2259711140b6
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-10-29 13:33:18 +0100

    Housekeeping

---
+++ housekeeping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Zur zyklischen Ausführung in cron etc.
+
+# Da cron die .profile nicht inkludiert, hier nachholen, explizit bash setzen vorher
+. /home/pi/.profile
+
+DIR=`dirname $0`
+cd $DIR
+
+LOG=log/housekeeping.log
+
+bin/rails runner -e production "Temperatur.housekeeping" 2>>$LOG >>$LOG
+RC=$?
+
+if [ $RC -ne 0 ]; then
+  echo `date`  >> $LOG
+  echo "Fehler bei Ausführung Temperatur.housekeeping" >> $LOG
+  echo "RC=$RC" >> $LOG
+fi
+exit $RC
