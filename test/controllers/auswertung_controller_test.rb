4ba0de11185d3cc2cbfb7336e5da12a4797b6923
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-05-13 21:52:48 +0200

    Test mit stubbing

--- test/controllers/auswertung_controller_test.rb
+++ test/controllers/auswertung_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class AuswertungControllerTest < ActionController::TestCase
-  test "should get show_temperatur_verlauf" do
+  test "show_temperatur_verlauf" do
     get :show_temperatur_verlauf
     assert_response :success
   end
 
+  test "show_temperatur_verlauf_aktuell" do
+    post :show_temperatur_verlauf_aktuell
+    assert_response :success
+  end
+
+
   test "list_temperatur_verlauf" do
-    post :list_temperatur_verlauf
+    post :list_temperatur_verlauf, :start=>"12.05.2016", :ende=>"13.05.2016"
     assert_response :success
   end
 
--- test/models/temperatur_test.rb
+++ test/models/temperatur_test.rb
@@ -1,11 +1,31 @@
 require 'test_helper'
 
 class TemperaturTest < ActiveSupport::TestCase
-  test "schreibe_temperatur" do
-    Temperatur.schreibe_temperatur(18,20)
-    sleep(1)
-    Temperatur.schreibe_temperatur(19,21)
+  setup do
+    ENV['FILENAME_VORLAUF']   = Rails.root.join('test', 'fixtures', 'vorlauf.sensor_file').to_s
+    ENV['FILENAME_RUECKLAUF'] = Rails.root.join('test', 'fixtures', 'ruecklauf.sensor_file').to_s
+    ENV['FILENAME_SCHATTEN']  = Rails.root.join('test', 'fixtures', 'schatten.sensor_file').to_s
+    ENV['FILENAME_SONNE']     = Rails.root.join('test', 'fixtures', 'sonne.sensor_file').to_s
 
-    assert Temperatur.find_each.count == 2
+    ENV['SCHALTER_TYP']       = 'Rutenbeck_TPIP1'
+    ENV['SCHALTER_IP']        = '127.0.0.0'
+
+    Temperatur.stubs(:get_schalter_status).returns(0)
+    Temperatur.stubs(:set_schalter_status)
+  end
+
+  teardown do
+    Temperatur.unstub(:get_schalter_status)
+    Temperatur.unstub(:set_schalter_status)
+
+  end
+
+  test "schreibe_temperatur" do
+    start_count = Temperatur.find_each.count
+    Temperatur.schreibe_temperatur
+    sleep(1)
+    Temperatur.schreibe_temperatur
+
+    assert Temperatur.find_each.count == start_count + 2
   end
 end
