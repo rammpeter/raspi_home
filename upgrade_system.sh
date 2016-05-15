decf3b5972d0e381d371478c9ac3ff2ca5ef73ae
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-05-15 14:11:37 +0200

    upgrade-script

--- upgrade_system.sh
+++ upgrade_system.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Zur Aktualisierung des Systems
+# Zur Aktualisierung des Systems ausführen im Apllikations-Verzeichnis
 RC=0
 
 # Aktualisierung der Software
 git pull
+
+# Abhängigkeiten nachladen
+bundle install
 
 # Anpassung der Datenbank an aktuelle Software
 rake db:migrate
