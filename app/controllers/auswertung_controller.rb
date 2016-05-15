669cd708cd82b84f1c5bada0dfdc246a85f1154d
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-05-15 15:00:04 +0200

    Auswertung der neuen Spalten

--- app/controllers/auswertung_controller.rb
+++ app/controllers/auswertung_controller.rb
@@ -35,19 +35,25 @@ class AuswertungController < ApplicationController
 
     @temps = Temperatur.where(:created_at => startDate..endeDate)
 
-    @vorlauf = ""
-    @ruecklauf = ""
-    @sonne = ""
-    @schatten = ""
-    @pumpenstatus = ""
+    @vorlauf                            = ""
+    @ruecklauf                          = ""
+    @sonne                              = ""
+    @schatten                           = ""
+    @pumpenstatus                       = ""
+    @wegen_temperatur_aktiv             = ''
+    @wegen_zirkulationszeit_aktiv       = ''
+    @wegen_zyklischer_reinigung_aktiv   = ''
 
     @temps.each do |t|
       milliseconds_since_1970 = t.created_at.to_i*1000 + t.created_at.utc_offset*1000
-      @vorlauf      << "[#{milliseconds_since_1970}, #{t.Vorlauf}],\n"
-      @ruecklauf    << "[#{milliseconds_since_1970}, #{t.Ruecklauf}],\n"
-      @sonne        << "[#{milliseconds_since_1970}, #{t.Sonne}],\n"
-      @schatten     << "[#{milliseconds_since_1970}, #{t.Schatten}],\n"
-      @pumpenstatus << "[#{milliseconds_since_1970}, #{t.Pumpenstatus == 1 ? 10 : 0}],\n"
+      @vorlauf                          << "[#{milliseconds_since_1970}, #{t.Vorlauf}],\n"
+      @ruecklauf                        << "[#{milliseconds_since_1970}, #{t.Ruecklauf}],\n"
+      @sonne                            << "[#{milliseconds_since_1970}, #{t.Sonne}],\n"
+      @schatten                         << "[#{milliseconds_since_1970}, #{t.Schatten}],\n"
+      @pumpenstatus                     << "[#{milliseconds_since_1970}, #{t.Pumpenstatus                     == 1 ? 5 : 0}],\n"
+      @wegen_temperatur_aktiv           << "[#{milliseconds_since_1970}, #{t.wegen_temperatur_aktiv           == 1 ? 5 : 0}],\n"
+      @wegen_zirkulationszeit_aktiv     << "[#{milliseconds_since_1970}, #{t.wegen_zirkulationszeit_aktiv     == 1 ? 5 : 0}],\n"
+      @wegen_zyklischer_reinigung_aktiv << "[#{milliseconds_since_1970}, #{t.wegen_zyklischer_reinigung_aktiv == 1 ? 5 : 0}],\n"
     end
 
   end
