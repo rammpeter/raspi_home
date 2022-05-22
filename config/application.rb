17810910dd13d91cd4992ee67c819268fcb384e3
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-05-22 14:29:40 +0200

    config.public_file_server.enabled = true

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module RaspiHome
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults 7.0
+
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -19,15 +19,6 @@ module RaspiHome
     config.active_record.default_timezone = :local
 
     #config.active_record.sqlite3.represent_boolean_as_integer = true
-
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    #config.active_record.raise_in_transactional_callbacks = true
-    #
     config.active_record.legacy_connection_handling = false
   end
 end
