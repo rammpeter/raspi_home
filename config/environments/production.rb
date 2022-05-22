3ee5163b63866db384154b150e28f6b92bffa3c5
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-05-22 14:44:49 +0200

    log level warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,5 +48,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.log_level = :debug
+  config.log_level = :warn
 end
